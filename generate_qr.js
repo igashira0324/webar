42ccf8b9131de236422e1595a9c9528b557bf4be
igashira0324 <igashira0324@users.noreply.github.com>
2026-05-01 18:41:24 +0900

    fix: right-align AR marker modal and use stable Vercel URL for QR to bypass preview login

--- generate_qr.js
+++ generate_qr.js
@@ -2,17 +2,17 @@ const QRCode = require('qrcode');
 
 QRCode.toFile(
   'public/assets/marker_qr.png',
-  'https://webar-qg96tqaxd-igashira0324s-projects.vercel.app/',
+  'https://webar-git-master-igashira0324s-projects.vercel.app/', // Fixed to stable master domain
   {
     color: {
-      dark: '#000000',  // Black dots
-      light: '#ffffff' // White background
+      dark: '#000000',
+      light: '#ffffff'
     },
     width: 500,
     margin: 4
   },
   function (err) {
     if (err) throw err;
-    console.log('QR code generated!');
+    console.log('QR code generated with stable URL!');
   }
 );
